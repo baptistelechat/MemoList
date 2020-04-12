2bf9cf15671b7f1b1981da441bd543d450562ba6
Baptiste LECHAT <baptistelechat@outlook.com>
2020-04-12 21:02:09 +0200

    🚧 Disable fragment view on landscape mode / Try to add the fragment on landscape view

--- app/src/main/java/com/monprojet/memolist/view/MemoAdapter.java
+++ app/src/main/java/com/monprojet/memolist/view/MemoAdapter.java
@@ -2,6 +2,8 @@ package com.monprojet.memolist.view;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,6 +12,9 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.gson.Gson;
@@ -18,6 +23,7 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 import com.monprojet.memolist.R;
 import com.monprojet.memolist.controller.DetailActivity;
+import com.monprojet.memolist.controller.DetailFragment;
 import com.monprojet.memolist.model.MemoDTO;
 import com.monprojet.memolist.model.json.Form;
 
@@ -63,23 +69,39 @@ public class MemoAdapter extends RecyclerView.Adapter<MemoAdapter.MemoViewHolder
         public MemoViewHolder(View itemView)
         {
             super(itemView);
+            final int orientation = itemView.getResources().getConfiguration().orientation;
             TextViewLabelMemo = itemView.findViewById(R.id.LabelMemo);
             TextViewLabelMemo.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(final View view) {
                     final MemoDTO memo = MemoList.get(getAdapterPosition());
-                    Log.i(TAG, "onClick: Position : "+getAdapterPosition());
+                    Log.i(TAG, "onClick: Position : " + getAdapterPosition());
                     //Toast.makeText(view.getContext(), memo.name, Toast.LENGTH_SHORT).show();
 
                     SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(view.getContext());
                     SharedPreferences.Editor editor = preferences.edit();
-                    editor.putString("memo",memo.name);
+                    editor.putString("memo", memo.name);
                     editor.apply();
 
-                    Intent intent = new Intent(view.getContext(), DetailActivity.class);
-                    intent.putExtra("memo", memo.name);
-                    view.getContext().startActivity(intent);
+                    // Load Fragment if ORIENTATION_PORTRAIT else ORIENTATION_LANDSCAPE
+                    if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+                        Intent intent = new Intent(view.getContext(), DetailActivity.class);
+                        intent.putExtra("memo", memo.name);
+                        view.getContext().startActivity(intent);
+                    }
 
+                    //----------------
+                    // ERROR
+                    //----------------
+//                    } else {
+//                        DetailFragment fragment = new DetailFragment();
+//                        // fragment manager :
+//                        FragmentManager fragmentManager = getSupportFragmentManager();
+//                        // transaction :
+//                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//                        fragmentTransaction.replace(R.id.fragmentLandscape, fragment, "exemple1");
+//                        fragmentTransaction.commit();
+//                    }
 
                     // client HTTP :
                     AsyncHttpClient client = new AsyncHttpClient();
@@ -122,20 +144,20 @@ public class MemoAdapter extends RecyclerView.Adapter<MemoAdapter.MemoViewHolder
         notifyItemInserted(MemoList.size());
     }
 
-    public boolean onItemMove(int positionDebut, int positionFin)
-    {
-        Collections.swap(MemoList, positionDebut, positionFin);
-        notifyItemMoved(positionDebut, positionFin);
-        return true;
-    }
-    // Appelé une fois à la suppression.
-    public void onItemDismiss(int position)
-    {
-        if (position > -1)
-        {
-            MemoList.remove(position);
-            notifyItemRemoved(position);
-        }
-    }
+//    public boolean onItemMove(int positionDebut, int positionFin)
+//    {
+//        Collections.swap(MemoList, positionDebut, positionFin);
+//        notifyItemMoved(positionDebut, positionFin);
+//        return true;
+//    }
+//    // Appelé une fois à la suppression.
+//    public void onItemDismiss(int position)
+//    {
+//        if (position > -1)
+//        {
+//            MemoList.remove(position);
+//            notifyItemRemoved(position);
+//        }
+//    }
 
 }
